1c9d58869821a6985b92e429724e41e5fd351afd
lucky-blessed <blessednkwor7@gmail.com>
2024-07-29 10:59:56 +0100

    fixing

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,18 +11,18 @@ class BasicCache(BaseCaching):
     """
        BasicCache is a caching system that inherits
        from the Basecaching. This caching system doen't
-       have any limit
+       have any limit.
     """
 
     def put(self, key, item):
         """
-        Add an item in the cache
+        Add an item in the cache.
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
         """
-            Get an item by key
+            Get an item by key.
         """
         return self.cache_data.get(key, None)
